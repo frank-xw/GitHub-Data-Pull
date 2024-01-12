2d1cff5e73f6a6ef69c142d536bd7f1888e7e77d
frank-xw <24924150+frank-xw@users.noreply.github.com>
2024-01-11 23:07:47 -0500

    construct issue dict

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
+from github import Github, Auth
+import os
+from dotenv import load_dotenv
 
-import requests
-# from github import Github, Auth
+# Authenticate and preparation
+load_dotenv()
 
-# github username
-username = "frank-xw"
-# url to request
-url = f"https://api.github.com/users/{username}"
-# make the request and return the json
-user_data = requests.get(url)
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 
-user_data.json()
+auth = Auth.Token(GITHUB_TOKEN)
+g = Github(auth=auth)
+
+# Define Repo
+repo = g.get_repo("frank-xw/GitHub-Data-Pull")
+issue = repo.get_issues(state='all')[0]
+
+# Construct Issue Dictionary
+issues = repo.get_issues()
+issues_dict = {}
+i = 1
+for issue in issues:
+    issue_dict = {}
+    issue_dict['url'] = issue.url
+    issue_dict['title'] = issue.title
+    issue_dict['body'] = issue.body
+    issue_dict['labels'] = issue.labels  # TODO: update to list
+    issue_dict['comments'] = [comment.body for comment in issue.get_comments()]
+    issues_dict[i] = issue_dict
+    i += 1
+
+print(issues_dict)
+
+g.close()
